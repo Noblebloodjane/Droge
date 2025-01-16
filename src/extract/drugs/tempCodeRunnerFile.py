8430a22762cbfc1ea96398480a77f8099220294f
Ifeoluwa Janet Ibukun-ojo <iibukunojo@gmail.com>
2025-01-16 01:50:21 +0100

    push

--- src/extract/drugs/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    print(f'This is the letter showing {letter(letter_count)}')
-    print(f'drug its reading {drugs}')
--- src/extract/drugs/tempstore.py
+++
@@ -1,5 +0,0 @@
-# Might change it to database later, not that it matters now
-Drugs = {
-    'Amoxicillin' : ['Amoxicillin','Amoxicillin Capsules','Amoxil', 'Amoxicillin Tablets'],
-    'Paracetamol' : ['Paracetamol', 'Panadol', 'pg']
-}
